c9faaa533373a3a9f5fbf2dbdff4a1b969c377d4
Thiago Alvarez <thiago.alvarez2012@gmail.com>
2023-06-29 15:53:28 -0300

    Creating SubMenus

---
+++ DotControls.WPF/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace DotControls.WPF;
+
+public sealed class Menu : ItemsControl
+{
+    public Menu()
+    {
+        this.DefaultStyleKey = typeof(Menu);
+    }
+}
---
+++ DotPharma.WPF/Controls/DropDownMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DotPharma.WPF.Controls;
+
+public class DropDownMenu : ItemsControl
+{
+    public DropDownMenu()
+    {
+        this.DefaultStyleKey = typeof(DropDownMenu);
+    }
+
+    public string Header
+    {
+        get { return (string)GetValue(HeaderProperty); }
+        set { SetValue(HeaderProperty, value); }
+    }
+
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register("Header", typeof(string), typeof(DropDownMenu), new PropertyMetadata(string.Empty));
+
+    protected override bool IsItemItsOwnContainerOverride(object item)
+    {
+        return item is RadioButton;
+    }
+
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+        return new StackPanel();
+    }
+
+}
