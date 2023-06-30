939ace89b214514c3bb88cf99f8309436d7e4a02
Thiago Alvarez <thiago.alvarez2012@gmail.com>
2023-06-29 21:34:19 -0300

    SubMenu finished

--- DotPharma.WPF/Controls/DropDownMenu.cs
+++ DotPharma.WPF/Controls/DropDownMenu.cs
@@ -20,6 +20,17 @@ public class DropDownMenu : ItemsControl
     public static readonly DependencyProperty HeaderProperty =
         DependencyProperty.Register("Header", typeof(string), typeof(DropDownMenu), new PropertyMetadata(string.Empty));
 
+
+    public string GroupName
+    {
+        get { return (string)GetValue(GroupNameProperty); }
+        set { SetValue(GroupNameProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for GroupName.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty GroupNameProperty =
+        DependencyProperty.Register("GroupName", typeof(string), typeof(DropDownMenu), new PropertyMetadata("defaultMenu"));
+
     protected override bool IsItemItsOwnContainerOverride(object item)
     {
         return item is RadioButton;
