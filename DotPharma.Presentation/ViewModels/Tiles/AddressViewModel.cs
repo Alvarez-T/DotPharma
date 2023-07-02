542551e61d35ff32f1e5230de6c87ab22e2a4b45
Thiago Alvarez <thiago.alvarez2012@gmail.com>
2023-07-02 11:31:12 -0300

    Creating some ViewModels

---
+++ DotPharma.Presentation/ViewModels/Tiles/AddressViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NavigatR.MVVM;
+
+namespace DotPharma.Presentation.ViewModels.Tiles;
+
+public partial class AddressViewModel : ObservableValidator, IViewModel
+{
+    [ObservableProperty]
+    private string _address;
+
+    [ObservableProperty]
+    private string _number;
+
+    [ObservableProperty]
+    private string _district;
+
+    [ObservableProperty]
+    private string _complement;
+
+    [ObservableProperty]
+    private string _city;
+
+    [ObservableProperty]
+    private string _uf;
+}
---
+++ DotPharma.Presentation/ViewModels/Tiles/PersonalInfoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DotPharma.Presentation.ViewModels.Tiles;
+
+public partial class PersonalInfoViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FullName))] 
+    private string _name;
+
+    [ObservableProperty] 
+    [NotifyPropertyChangedFor(nameof(FullName))]
+    private string _lastName;
+
+    public string FullName => $"{Name} {LastName}";
+
+    [ObservableProperty] private string _email;
+    [ObservableProperty] private string _phone;
+    [ObservableProperty] private string _cpf;
+    [ObservableProperty] private string _rg;
+}
