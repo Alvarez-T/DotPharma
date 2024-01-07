fc4954ee8e9345b6b90e9d809f002a6b6275ed56
Alvarez-T <thiago.alvarez2012@gmail.com>
2024-01-07 00:42:36 -0300

    Avalonia Branch unified

---
+++ DotPharma.Avalonia.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace DotPharma.Avalonia.Desktop
+{
+    public partial class MainWindow : SukiWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+    }
+}
