86894805c57b198d1f4d2fb708da30684667c013
Alvarez-T <thiago.alvarez2012@gmail.com>
2024-06-12 20:01:33 -0300

    Created new project for PDV.

---
+++ DotPharma.Avalonia.PDV/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DotPharma.Avalonia.PDV;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
